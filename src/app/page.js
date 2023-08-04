107b3c8a633d03a6636845b833faddf55beac726
Bharat Kushwah <bharatkushwah407@gmail.com>
2023-08-04 20:04:21 +0530

    update2

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,73 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className=" text-black  ">HEllo Ji</h1>
-    </main>
+    <div class="App-header">
+      <div class="skillcard" id="index">
+        <div class="header text-center">
+          <span class="hireButton">
+            <a href="#home">
+              <h4>Home</h4>
+            </a>
+          </span>
+          <span class="hireButton">
+            <a href="#about">
+              <h4>About Me</h4>
+            </a>
+          </span>
+          <span class="hireButton">
+            <a href="#edu">
+              <h4>Education</h4>
+            </a>
+          </span>
+          <span class="hireButton">
+            <a href="#project">
+              <h4>Projects</h4>
+            </a>
+          </span>
+          <span class="hireButton">
+            <a href="#contact">
+              <h4>Contact Me</h4>
+            </a>
+          </span>
+        </div>
+        <div class="description text-center">
+          <span class="hireButton">
+            <a href="#dow">
+              <h4>INDEX</h4>
+            </a>
+          </span>
+        </div>
+      </div>
+      <div class="lorem">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        nulla ex maxime architecto fugiat, a, eius quibusdam rem sit est officia
+        non ad ipsum animi culpa iusto quas porro. Soluta? Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Commodi ad nam consectetur autem
+        vitae fugiat fugit ex quos nobis libero. Consequuntur dolorem eum
+        ducimus nam rerum at delectus aliquid blanditiis. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Voluptatibus nulla ex maxime
+        architecto fugiat, a, eius quibusdam rem sit est officia non ad ipsum
+        animi culpa iusto quas porro. Soluta? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Commodi ad nam consectetur autem vitae
+        fugiat fugit ex quos nobis libero. Consequuntur dolorem eum ducimus nam
+        rerum at delectus aliquid blanditiis. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Voluptatibus nulla ex maxime architecto
+        fugiat, a, eius quibusdam rem sit est officia non ad ipsum animi culpa
+        iusto quas porro. Soluta? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Commodi ad nam consectetur autem vitae fugiat fugit ex
+        quos nobis libero. Consequuntur dolorem eum ducimus nam rerum at
+        delectus aliquid blanditiis. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Voluptatibus nulla ex maxime architecto fugiat, a,
+        eius quibusdam rem sit est officia non ad ipsum animi culpa iusto quas
+        porro. Soluta? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Commodi ad nam consectetur autem vitae fugiat fugit ex quos nobis
+        libero. Consequuntur dolorem eum ducimus nam rerum at delectus aliquid
+        blanditiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        nulla ex maxime architecto fugiat, a, eius quibusdam rem sit est officia
+        non ad ipsum animi culpa iusto quas porro. Soluta? Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Commodi ad nam consectetur autem
+        vitae fugiat fugit ex quos nobis libero. Consequuntur dolorem eum
+        ducimus nam rerum at delectus aliquid blanditiis.
+      </div>
+    </div>
   );
 }
--- src/models/a.js
+++ src/models/a.js
@@ -1,6 +1,10 @@
-var i = 0;
-for (var i = 0; i < 5; i++) {
-  setTimeout(() => {}, 2000);
+let i = 0;
+for (let i = 0; i < 5; i++) {
+  setTimeout(() => {
+    console.log("my no is :- ", i);
+  }, 2000);
+  
 }
 
 console.log(i);
+555
